6cf3680975bd9bab3fd5562c4f3b6be20e7a9290
Nathan Yin <nathanzjyin@gmail.com>
2022-12-23 01:28:30 +1100

    defined Submission, Author, Subreddit models

--- src/memefeed/reddit/models.py
+++ src/memefeed/reddit/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    """
+    Author of a submission.
+    """
+    name = models.CharField(primary_key=True, max_length=20)
+    favourite = models.BooleanField(default=False)
+    def __str__(self) -> str:
+        return self.name
+
+class Subreddit(models.Model):
+    """
+    Subreddit.
+    """
+    name = models.CharField(primary_key=True, max_length=21)
+    favourite = models.BooleanField(default=False)
+
+class Submission(models.Model):
+    """
+    Submission made on a subreddit by an Author.
+    """
+    id = models.CharField("Reddit post's ID", max_length=6, primary_key=True)
+    title = models.CharField("Title of a Reddit post", max_length=300)
+    score = models.IntegerField("Number of upvotes on a reddit post")
+    url = models.URLField("Submission URL")
+    domain = models.CharField("Domain", max_length=50)
+    created_utc = models.DateTimeField("Date the submission was created")
+    # Submission Content / Media
+    # TODO: Normalize media
+    is_self = models.BooleanField()
+    is_video = models.BooleanField()
+    media = models.JSONField(default=None, blank=True, null=True)
+    media_embed = models.JSONField(default=None, blank=True, null=True)
+    selftext = models.TextField(max_length=40000, default=None, blank=True, null=True)
+    selftext_html = models.TextField(default=None, blank=True, null=True)  # TODO: Should this be binary?
+    nsfw = models.BooleanField()
+    thumbnail = models.URLField("Thumbnail URL")
+    secure_media = models.JSONField(default=None, blank=True, null=True)
+    secure_media_embed = models.JSONField(default=None, blank=True, null=True)
+    # Foreign Keys
+    subreddit = models.ForeignKey(Subreddit, models.CASCADE)
+    author = models.ForeignKey(Author, models.CASCADE)
+
+
+
