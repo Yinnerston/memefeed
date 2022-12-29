c2ef8af979d467f79abdc2c08eadd173872eb73a
Nathan Yin <nathanzjyin@gmail.com>
2022-12-30 01:27:46 +1100

    added sort_by choices <select> to search

--- src/memefeed/reddit/forms.py
+++ src/memefeed/reddit/forms.py
@@ -1,7 +1,8 @@
 # https://docs.djangoproject.com/en/4.1/topics/forms/
 from django import forms
 
-sort_choices = ["relevence", "score", "new", "A-Z"]
+# Enumerated choices
+sort_choices = [(1, "relevence"), (2, "score"), (3, "new"), (4, "A-Z")]
 
 class SearchForm(forms.Form):
     template_name = "search_form.html"
@@ -9,7 +10,7 @@ class SearchForm(forms.Form):
     # TODO: Migrate to forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=...)
     subreddit = forms.CharField(label="r/subreddit", max_length=21)
     author = forms.CharField(label="u/author", max_length=20)
-    sort_by = forms.Select(choices=sort_choices)
+    sort_by = forms.ChoiceField(choices=sort_choices)
 
     def make_results(self):
         pass
