c4b9afc71994abc1c062d285db22627f41f3739e
Nathan Yin <nathanzjyin@gmail.com>
2022-12-30 01:36:24 +1100

    added multiple choice checkboxes for all subreddits

--- src/memefeed/reddit/forms.py
+++ src/memefeed/reddit/forms.py
@@ -1,14 +1,16 @@
 # https://docs.djangoproject.com/en/4.1/topics/forms/
 from django import forms
-
-# Enumerated choices
-sort_choices = [(1, "relevence"), (2, "score"), (3, "new"), (4, "A-Z")]
+from .models import Subreddit
 
 class SearchForm(forms.Form):
+    # Enumerated choices
+    sort_choices = [(0, "relevence"), (1, "score"), (2, "new"), (3, "A-Z")]
+    subreddit_choices = zip(range(Subreddit.objects.count()), Subreddit.objects.all())
+
+    # Fields in the form
     template_name = "search_form.html"
     q = forms.CharField(label="Search Term", max_length=300)
-    # TODO: Migrate to forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=...)
-    subreddit = forms.CharField(label="r/subreddit", max_length=21)
+    subreddit = forms.MultipleChoiceField(label="r/subreddit", widget=forms.CheckboxSelectMultiple, choices=subreddit_choices)
     author = forms.CharField(label="u/author", max_length=20)
     sort_by = forms.ChoiceField(choices=sort_choices)
 
