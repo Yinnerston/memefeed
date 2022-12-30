5e92e6689e64410fa15bd88d06ee4ad44fee1ef9
Nathan Yin <nathanzjyin@gmail.com>
2022-12-31 06:10:29 +1100

    added Admin for submission, subreddit, author including more verbose display in listview and search

--- src/memefeed/reddit/admin.py
+++ src/memefeed/reddit/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from .models import Author, Subreddit, Submission
 
 # Register your models here.
-admin.site.register(Author)
-admin.site.register(Subreddit)
-admin.site.register(Submission)
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'favourite')
+    search_fields = ['name', 'favourite']
+
+@admin.register(Subreddit)
+class SubredditAdmin(admin.ModelAdmin):
+    list_display = ('name', 'favourite')
+    search_fields = ['name', 'favourite']
+
+
+@admin.register(Submission)
+class SubmissionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'subreddit', 'author', 'score', 'url')
+    search_fields = ['title', 'subreddit__name', 'author__name']
+
