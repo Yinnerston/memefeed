94bae1a0136db2b2ff2e4fd40184ac10700872e6
Nathan Yin <nathanzjyin@gmail.com>
2023-01-16 00:17:27 +1100

    added built-in file checking to precommit memefeed-collectstatic hook.

--- memefeed-collectstatic.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-if git status --short | grep static; then
-    # docker-compose -f docker-compose.prod.yml exec memefeed python manage.py collectstatic --no-input --clear
-    echo "MATCH"
-fi
