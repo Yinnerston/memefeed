abab0d1066e24e825e92ce9d71588f7cee156760
Nathan Yin <nathanzjyin@gmail.com>
2023-01-16 00:12:39 +1100

    renamed collecstatic precommit hook to memefeed-collectstatic and made it a bash script

--- memefeed-collectstatic.sh
+++ memefeed-collectstatic.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 if git status --short | grep static; then
     # docker-compose -f docker-compose.prod.yml exec memefeed python manage.py collectstatic --no-input --clear
     echo "MATCH"
