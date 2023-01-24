c88138aec02994a88e4eda4076e0ab917a3b7e7c
Nathan Yin <nathanzjyin@gmail.com>
2023-01-25 02:38:25 +1100

    added implementation for author view

--- src/memefeed/reddit/urls.py
+++ src/memefeed/reddit/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import IndexView, SearchView, SearchResultsView, SubredditView
+from .views import IndexView, SearchView, SearchResultsView, SubredditView, AuthorView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.decorators.cache import cache_page
@@ -10,6 +10,7 @@ urlpatterns = [
     path(
         "subreddit/<str:subreddit_name>", SubredditView.as_view(), name="subreddit_view"
     ),
+    path("author/<str:author_name>", AuthorView.as_view(), name="author_view"),
     path("search/results", SearchResultsView.as_view(), name="search_results"),
     path("search/", SearchView.as_view(), name="compose_search"),
     path("", cache_page(60 * 30)(IndexView.as_view()), name="index"),
