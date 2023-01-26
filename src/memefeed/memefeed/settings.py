b35077a61d6fa73f5a4297153f48345392556f1a
Nathan Yin <nathanzjyin@gmail.com>
2023-01-27 01:59:49 +1100

    add nathanmemefeed to allowed hosts

--- src/memefeed/memefeed/settings.py
+++ src/memefeed/memefeed/settings.py
@@ -53,7 +53,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("DJANGO_DEV_SECRET_KEY")
 
 
-ALLOWED_HOSTS = ["localhost", "memefeed", ".memefeed.xyz", os.getenv("PROD_IP")]
+ALLOWED_HOSTS = ["localhost", "memefeed", ".memefeed.xyz", "nathanmemefeedserver",os.getenv("PROD_IP")]
 
 
 # Application definition
