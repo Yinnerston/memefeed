235e15d6954a790515d2a9cb8ba89220db15e07f
Nathan Yin <nathanzjyin@gmail.com>
2023-01-31 17:59:55 +1100

    added nathanmemefeedserver dns to domains in certificate

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose -f docker-compose.prod.yml)" ]; then
   exit 1
 fi
 
-domains=(memefeed.xyz)
+domains=(memefeed.xyz nathanmemefeedserver)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="nathan.memefeed@gmail.com" # Adding a valid address is strongly recommended
